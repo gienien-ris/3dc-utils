1ed7a4a919bf0f8999387a5001149267b8decf44
gienien-ris <118286564+gienien-ris@users.noreply.github.com>
2026-02-27 14:23:34 +0100

    Update vms_portgroups_vlans.ps1

--- vms_portgroups_vlans.ps1
+++ vms_portgroups_vlans.ps1
@@ -1,18 +1,23 @@
-Get-VM  | ForEach-Object {
+Get-VM -name "*stefan*" | ForEach-Object {
 
     $vm = $_
 
     Get-NetworkAdapter -VM $vm | ForEach-Object {
         $nic = $_
         $pgName = $nic.NetworkName
-        $pg = Get-VDPortGroup -Name $pgName
+        $pg = Get-VDPortGroup -Name $pgName 
+        if ($pg) {
+          
         if ($pg.ExtensionData.Config.DefaultPortConfig.Vlan.VlanId -is [int]) {
             $vlan = ($pg.ExtensionData.Config.DefaultPortConfig.Vlan.VlanId)
         }
         else {
             $vlan = $pg.VlanConfiguration.Ranges
         }
-        
+    } else { 
+        $pg = Get-VirtualPortGroup -Name $pgName
+        $vlan = $pg.VlanId
+    }
         #Write-Host $vm $nic $pgName $vlan $switch
         [PSCustomObject]@{
             VMName         = $vm.Name
@@ -20,8 +25,5 @@ Get-VM  | ForEach-Object {
             Portgroup      = $pgName
             VlanId         = $vlan
         }
-
-
     }
-
-} | export-csv -Path C:\git\powershell\stefan_dc3_utils\vm_nic_vlans.csv -NoTypeInformation -Encoding unicode
+} | export-csv -Path C:\sb\vm_nic_vlans.csv -NoTypeInformation -Encoding unicode
